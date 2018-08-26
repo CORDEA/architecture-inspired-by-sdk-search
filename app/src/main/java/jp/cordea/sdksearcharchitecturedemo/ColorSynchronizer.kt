bc3ef63be4fa66a1653684f0dc035809af53d0e5
Yoshihiro Tanaka <contact@cordea.jp>
2018-08-26 13:47:42 +0900

    use data source

--- app/src/main/java/jp/cordea/sdksearcharchitecturedemo/ColorSynchronizer.kt
+++ app/src/main/java/jp/cordea/sdksearcharchitecturedemo/ColorSynchronizer.kt
@@ -2,27 +2,48 @@ package jp.cordea.sdksearcharchitecturedemo
 
 import kotlinx.coroutines.experimental.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.experimental.channels.ReceiveChannel
-import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
-import java.util.*
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class ColorSynchronizer @Inject constructor() {
-    private val random: Random = Random()
-    private val mutableState: ConflatedBroadcastChannel<SyncState> = ConflatedBroadcastChannel()
+class ColorSynchronizer @Inject constructor(
+        private val remoteDataSource: ColorRemoteDataSource
+) {
     val state: ReceiveChannel<SyncState> get() = mutableState.openSubscription()
+    val result: ReceiveChannel<ColorResult> get() = mutableResult.openSubscription()
 
-    fun sync() {
-        mutableState.offer(SyncState.SYNC)
-        launch {
-            mutableState.offer(if (tryFetch()) SyncState.COMPLETED else SyncState.FAILED)
+    private var cachedItem: ColorFetchResult.Success? = null
+    private val mutableState = ConflatedBroadcastChannel<SyncState>()
+    private val mutableResult = ConflatedBroadcastChannel<ColorResult>()
+
+    fun sync(action: ColorAction) {
+        when (action) {
+            ColorAction.Initialize -> {
+                mutableState.offer(SyncState.SYNC)
+                fetchColor()
+            }
+            is ColorAction.Filter -> filterColor(action.query)
         }
     }
 
-    private suspend fun tryFetch(): Boolean {
-        delay(random.nextInt(4).toLong(), TimeUnit.SECONDS)
-        return random.nextBoolean()
+    private fun fetchColor() = launch {
+        val result = remoteDataSource.tryFetch()
+        when (result) {
+            is ColorFetchResult.Success -> {
+                mutableResult.send(result)
+                cachedItem = result
+                mutableState.send(SyncState.COMPLETED)
+            }
+            ColorFetchResult.Failure ->
+                mutableState.send(SyncState.FAILED)
+        }
+    }
+
+    private fun filterColor(query: String) = launch {
+        cachedItem?.let {
+            mutableResult.send(it.copy(
+                    colors = it.colors.filter { it.name.startsWith(query) }
+            ))
+        }
     }
 
     enum class SyncState {
